4cd5550f7894cc9a635c4a3f3443ed09c2e0a478
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-14 00:26:31 +0000

    Edit article with author functionality

--- src/pages/ArticleEdit/ArticleEdit.js
+++ src/pages/ArticleEdit/ArticleEdit.js
@@ -1,72 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useEffect } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-import { ROUTE_ARTICLE_LIST } from '../../constants';
-import { getArticle, editArticle } from '../../services/articles';
-import RegionDropdown from '../../components/RegionDropdown/RegionDropdown';
+import { ROUTE_ARTICLE_LIST } from "../../constants";
+import { getArticle, editArticle } from "../../services/articles";
+import RegionDropdown from "../../components/RegionDropdown/RegionDropdown";
+import AuthorDropdown from "../../components/AuthorDropdown/AuthorDropdown";
 
 function ArticleEdit(props) {
-    const history = useHistory();
-    const { articleId } = useParams();
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [regions, setRegions] = useState([]);
+  const history = useHistory();
+  const { articleId } = useParams();
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState(null);
+  const [content, setContent] = useState("");
+  const [regions, setRegions] = useState([]);
 
-    useEffect(() => {
-        const fetchArticle = async () => {
-            const data = await getArticle(articleId);
-            setTitle(data.title);
-            setContent(data.content);
-            setRegions(data.regions);
-        };
-
-        fetchArticle();
-    }, [articleId]);
-
-    const handleSave = async () => {
-        const payload = { title, content, regions };
-        await editArticle(articleId, payload);
-        history.push(ROUTE_ARTICLE_LIST);
+  useEffect(() => {
+    const fetchArticle = async () => {
+      const data = await getArticle(articleId);
+      setTitle(data.title);
+      setAuthor(data.author);
+      setContent(data.content);
+      setRegions(data.regions);
     };
 
-    return (
-        <div className="ArticleEdit">
-            <h1>Edit Article</h1>
-            <Form>
-                <Form.Group>
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Title"
-                        value={ title }
-                        onChange={ (event) => setTitle(event.target.value) }
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Content</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        placeholder="Content"
-                        rows="5"
-                        value={ content }
-                        onChange={ (event) => setContent(event.target.value) }
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Regions</Form.Label>
-                    <RegionDropdown
-                        value={ regions }
-                        onChange={ (regions) => setRegions(regions) }
-                    />
-                </Form.Group>
-                <Button variant="primary" onClick={ handleSave }>
-                    Save Article
-                </Button>
-            </Form>
-        </div>
-    );
+    fetchArticle();
+  }, [articleId]);
+
+  const handleSave = async () => {
+    const payload = { title, author, content, regions };
+    debugger;
+    await editArticle(articleId, payload);
+    history.push(ROUTE_ARTICLE_LIST);
+  };
+
+  return (
+    <div className="ArticleEdit">
+      <h1>Edit Article</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Author</Form.Label>
+          <AuthorDropdown
+            value={author}
+            onChange={(selectedInput) => setAuthor(selectedInput)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Content</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Content"
+            rows="5"
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Regions</Form.Label>
+          <RegionDropdown
+            value={regions}
+            onChange={(regions) => setRegions(regions)}
+          />
+        </Form.Group>
+        <Button variant="primary" onClick={handleSave}>
+          Save Article
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default ArticleEdit;
