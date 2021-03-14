2308e0617a268f1086e10c9d95c904df5e328bc9
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-03-14 16:37:06 +0000

    Add author when listing articles

--- src/pages/ArticleList/ArticleList.js
+++ src/pages/ArticleList/ArticleList.js
@@ -1,52 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Table from 'react-bootstrap/Table';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Table from "react-bootstrap/Table";
 
-import { ROUTE_ARTICLE_PREFIX, ROUTE_ARTICLE_CREATE } from '../../constants';
-import { listArticles } from '../../services/articles';
+import { ROUTE_ARTICLE_PREFIX, ROUTE_ARTICLE_CREATE } from "../../constants";
+import { listArticles } from "../../services/articles";
 
 function ArticleList() {
-    const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([]);
 
-    useEffect(() => {
-        const fetchArticles = async () => {
-            const data = await listArticles();
-            setArticles(data);
-        };
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const data = await listArticles();
+      setArticles(data);
+    };
 
-        fetchArticles();
-    }, []);
+    fetchArticles();
+  }, []);
 
-    const renderArticles = () => articles.map((article) => {
-        const { id, title } = article;
+  const renderArticles = () =>
+    articles.map((article) => {
+      const { id, title, author } = article;
 
-        return (
-            <tr key={ id }>
-                <td>
-                    <Link to={ `${ROUTE_ARTICLE_PREFIX}/${id}` }>{ title }</Link>
-                </td>
-            </tr>
-        );
+      return (
+        <tr key={id}>
+          <td>
+            <Link to={`${ROUTE_ARTICLE_PREFIX}/${id}`}>{`${title} ${
+              author ? " by " + author.firstName + " " + author.lastName : ""
+            }`}</Link>
+          </td>
+        </tr>
+      );
     });
 
-    return (
-        <div className="ArticleList">
-            <h1>Articles</h1>
-            <Link className="d-block mb-3" to={ ROUTE_ARTICLE_CREATE }>
-                Create a new Article
-            </Link>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { renderArticles() }
-                </tbody>
-            </Table>
-        </div>
-    );
+  return (
+    <div className="ArticleList">
+      <h1>Articles</h1>
+      <Link className="d-block mb-3" to={ROUTE_ARTICLE_CREATE}>
+        Create a new Article
+      </Link>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{renderArticles()}</tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default ArticleList;
